Fixed zero pole plot 'non-causal' message

Zero_Pole_Plot/Zero_Pole_Plot_Continuous_utils.py
@@ -532,7 +532,7 @@ class DraggableZeroPolePlot(DraggableMarker):
             self.calc_not_possible()
 
         #This is to check if any pole is on/outside of the unit circle   
-        if np.any(np.abs(p)>=1):
+        if np.any(np.real(p) > 0):
             self.tx_deb.set_text("Filter is non causal")
         else:
             self.tx_deb.set_text("")
@@ -614,12 +614,12 @@ class DraggableZeroPolePlot(DraggableMarker):
                     self.actual_change = True
 
     def calc_not_possible(self):
-        self.cnp_gain = self.axs[1].text(0.5, 0.5, "Le calcul n'est pas possible", fontdict={'color': 'red', 'size': 17},
+        self.cnp_gain = self.axs[1].text(0.5, 0.5, "Calculation not possible", fontdict={'color': 'red', 'size': 17},
                                          horizontalalignment='center',
                                          verticalalignment='center',
                                          transform=self.axs[1].transAxes)
         if self.show_phase:
-            self.cnp_ph = self.axs[2].text(0.5, 0.5, "Le calcul n'est pas possible", fontdict={'color': 'red', 'size': 17},
+            self.cnp_ph = self.axs[2].text(0.5, 0.5, "Calculation not possible", fontdict={'color': 'red', 'size': 17},
                                            horizontalalignment='center',
                                            verticalalignment='center',
                                            transform=self.axs[2].transAxes)